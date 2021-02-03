4fa8cf80b8853565aabed04a0f35accee8f1e3c5
shubhangi44-coder <78415001+shubhangi44-coder@users.noreply.github.com>
2021-02-03 22:30:33 +0530

    Use of Inline function

---
+++ inline_function.cpp
@@ -0,0 +1,17 @@
+/*WAP to print product using inline function*/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+inline int product(int a, int b){
+    return a*b;
+}
+
+int main(){
+    int a,b;
+    cout<<"Enter the value of a and b"<<endl;
+    cin>>a>>b;
+    cout<<"The product of a and b is"<<product(a,b);
+    return 0;
+
+}
