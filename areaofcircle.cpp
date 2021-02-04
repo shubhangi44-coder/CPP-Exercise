977a7ee0337f54b097d06ac6890ebc920afa2282
shubhangi44-coder <78415001+shubhangi44-coder@users.noreply.github.com>
2021-02-04 11:51:59 +0530

    function without argument and with return value

--- areaofcircle.cpp
+++ areaofcircle.cpp
@@ -1,18 +1,21 @@
-/* WAP to print area of circle using function with argument and with return type */
+/* WAP to print area of circle using function with no argument and with return type */
 
 #include<iostream>
 #include<conio.h>
 using namespace std;
-int fun(int r);
+int fun();
 
 int main(){
-    float a=fun(4);
+    float a=fun();
     cout<<"The area of circle is "<<a;
     return 0; 
 }
 
 
-int fun(int r){
+int fun(){
+    int r;
+    cout<<"Enter the radius:"<<endl;
+    cin>>r;
     float area = 3.14*r*r;
     cout<<area<<endl;
     return area;
