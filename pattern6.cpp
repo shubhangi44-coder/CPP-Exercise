dba31fbee940ae96f02b10a92f2f2f89d98dfd75
shubhangi44-coder <78415001+shubhangi44-coder@users.noreply.github.com>
2021-02-10 22:57:07 +0530

    pattern example6

---
+++ pattern6.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+class Pattern{
+    public:
+    int line = 4;
+    int count = 5;
+    char a='A';
+    
+    void display(){
+        for (int i = 1; i<=line; i++){
+            for(int j = 1; j<=count; j++){
+                cout<<a;
+                a++; 
+            }
+            cout<<endl;
+            a='A';
+        }
+    }
+};
+int main(){
+    Pattern p;
+    p.display();
+}
