de9965c1bc1e12b6a7378de50394777f42bd8bae
shubhangi44-coder <78415001+shubhangi44-coder@users.noreply.github.com>
2021-02-10 17:17:43 +0530

    Pattern example2

---
+++ pattern2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+class Pattern{
+    public:
+    int line = 3;
+    int count = 5;
+    int num=1;
+    
+    void display(){
+        for (int i = 1; i<=line; i++){
+            for(int j = 1; j<=count; j++){
+                cout<<num; 
+            }
+            cout<<endl;
+            num++;
+        }
+    }
+};
+int main(){
+    Pattern p;
+    p.display();
+}
