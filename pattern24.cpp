f4e55ebbe8b60c0f2321dcc1fb76cf5573cfbce3
shubhangi44-coder <78415001+shubhangi44-coder@users.noreply.github.com>
2021-02-12 12:28:48 +0530

    pattern example24

---
+++ pattern24.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class Pattern
+{
+public:
+    void fun()
+    {
+        for (int i = 1; i <= 5; i++){
+
+            for (int j = 1; j <= 5; j++){
+                if (i==1 || i==5 || j==1 || j==5){
+                    cout<<"*"<<" ";
+                }
+                else{
+                    cout<<" "<<" ";
+                }   
+            }
+           cout <<"\n"; 
+        }  
+    }
+};
+int main()
+{
+    Pattern p;
+    p.fun();
+}
