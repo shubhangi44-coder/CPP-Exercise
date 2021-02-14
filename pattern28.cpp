f9e05c12b8f2da99f92a65819c4012d3ee02bc82
shubhangi44-coder <78415001+shubhangi44-coder@users.noreply.github.com>
2021-02-14 13:09:02 +0530

    pattern example28

---
+++ pattern28.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+class Pattern
+{
+public:
+    int line=6;
+    int spacecount=5;
+    int starcount=1;
+    void fun(){
+        for(int i=1; i<=line; i++){
+            for(int j=1; j<=spacecount; j++){
+                cout<<" "<<" ";
+            }
+            for(int k=1; k<=starcount; k++){
+                    cout<<"*"<<" ";
+            }
+            cout<<endl;
+            spacecount--;
+            starcount=starcount+1;
+        }
+    }  
+};
+int main()
+{
+    Pattern p;
+    p.fun();
+}
